c2b58c3d0a2f3c96efe750a260671c69c5ffd53a
vannuth chhorn <vannuthchhorn@gmail.com>
2020-09-04 12:47:43 +0700

    ASP.NET Core concept MVC

---
+++ AppEmployee/Data/AppEmployeeContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AppEmployee.Models;
+
+namespace AppEmployee.Data
+{
+    public class AppEmployeeContext : DbContext
+    {
+        public AppEmployeeContext (DbContextOptions<AppEmployeeContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AppEmployee.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ AppEmployee/Models/MovieGenreViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppEmployee.Models
+{
+    public class MovieGenreViewModel
+    {
+        public List<Movie> Movies { get; set; }
+        public SelectList Genres { get; set; }
+        public string MovieGenre { get; set; }
+        public string SearchString { get; set; }
+    }
+}
